8a0caefcb4c1503f12f99600b59c4e11223ef738
tanjinajui <tanjinaakter994@gmail.com>
2019-07-03 20:42:57 +0600

    StudentEntryApp

---
+++ StudentEntryAppExample2/StudentEntryAppExample2/Department.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEntryAppExample2
+{
+    class Department
+    {
+        public Department()
+        {
+            students = new List<Student>();
+        }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        private List<Student> students;
+
+        public bool AddStudent(Student student)
+        {
+            if (students.Count < 15)
+            {
+                students.Add(student);
+                return true;
+            }
+            return false;
+        }
+
+
+        public string GetDetails()
+        {
+            
+            string deptInfo = "Code: " + Code + " Name: " + Name;
+            string header = "RegNo \t\t Name \t\t Email";
+            string studentDetails = "";
+            foreach (Student student in students)
+            {
+                studentDetails += student.RegNo + " \t\t " + student.Name + " \t\t " + student.Email+Environment.NewLine;
+            }
+
+            string message = deptInfo + Environment.NewLine + header + Environment.NewLine + studentDetails;
+
+            return message;
+
+        }
+    }
+}
---
+++ StudentEntryAppExample2/StudentEntryAppExample2/StudentEntryUi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentEntryAppExample2
+{
+    public partial class StudentEntryUi : Form
+    {
+        public StudentEntryUi()
+        {
+            InitializeComponent();
+        }
+
+        private Department department;
+        private void DepartmentSaveButton_Click(object sender, EventArgs e)
+        {
+            department = new Department();
+            department.Code = codeTextBox.Text;
+            department.Name = departmentNameTextBox.Text;
+
+        }
+
+        private void StudentSaveButton_Click(object sender, EventArgs e)
+        {
+            if (department == null)
+            {
+                MessageBox.Show("Please Create Department First!");
+                return;
+            }
+
+            Student student = new Student();
+
+            student.Name = studentNameTextBox.Text;
+            student.RegNo = regNoTextBox.Text;
+            student.Email = emailTextBox.Text;
+
+            bool isAdded = department.AddStudent(student);
+            if (isAdded)
+            {
+                studentNameTextBox.Text = "";
+                regNoTextBox.Text = "";
+                emailTextBox.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Department Capacity Full!");
+            }
+        }
+
+        private void ShowDetailsButton_Click(object sender, EventArgs e)
+        {
+            string message = department.GetDetails();
+            MessageBox.Show(message);
+        }
+    }
+}
